0790faad218dbd1e0b8686c89c764f364fdb92f5
Mipster <tomaviv57@gmail.com>
2022-11-18 17:38:14 +0200

    Added row class

--- app/frontend/public/js/admin/table.js
+++ app/frontend/public/js/admin/table.js
@@ -1,5 +1,83 @@
 import $ from 'jquery';
 
+class Row extends HTMLTableRowElement {
+	table;
+	data;
+
+	constructor(table, data) {
+		super();
+
+		this.table = table;
+		this.data = data;
+
+		$(this).append(
+			Object
+				.entries(data)
+				.map(([key, value]) => this.generateCell(data, key, value))
+		);
+	}
+
+
+	generateCell(object, key, value) {
+		const instance = this.table;
+
+		const container = $('<td>');
+		const input = $('<input>')
+			.attr('type', 'text');
+
+		return container
+			.text(value.toString())
+			.on('click', function() {
+				const parent = $(this).parent();
+				if(!parent.prop('edit')) {
+					parent.prop('edit', true);
+
+					container.empty();
+					container.append(
+						input
+							.attr('value', value.toString())
+							.on('keydown', e => {
+								switch(e.key) {
+								case 'Enter':
+									if(object[key] !== input.val()) {
+										const copy = {...object};
+										copy[key] = input.val();
+
+										instance
+											.update(copy)
+											.then((response) => {
+												if (!response.ok) {
+													throw new Error('Network response was not OK');
+												}
+
+												object[key] = input.val();
+											})
+											.catch(() => { alert(`Failed to update field ${key}`); })
+											.then(() => {
+												container.empty();
+												container.text(object[key]);
+
+												parent.prop('edit', false);
+											});
+									}
+									break;
+								case 'Escape':
+									container.empty();
+									container.text(object[key]);
+
+									parent.prop('edit', false);
+								}
+							})
+					);
+					input.trigger('focus');
+				}
+			});
+	}
+}
+
+window.customElements.define('admin-tr', Row, {extends: 'tr'});
+
+
 export class Table {
 	#api;
 	#element;
@@ -13,7 +91,7 @@ export class Table {
 		this.#element = $('<table>')
 			.append(this.generateHeaders(Object.keys(objects[0])))
 			.append($('<tbody>')
-				.append(objects.map(o => this.generateRow(o)))
+				.append(objects.map(o => new Row(this, o)))
 			);
 	}
 
